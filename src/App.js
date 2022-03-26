ea8f7bbf50f96858ad61875ebf4b0bc44a7d662e
owakeelAhmmed <mdowakeelahmed5678@gmail.com>
2022-03-26 21:36:33 +0600

    Add to bonus

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ function App() {
   return (
     <div className="App">
      <Shop/>
+
+     <div className="card-info">
+       <h2>How React Works</h2>
+       <p> hreact js একটি Open-Source JavaScript Library. এর সাহায্যে Component তৈরি করা যায়। React Js এর মেইন ফিচার হলো Component. একটি Component একাধিক বার ব্যাবহার করা যায়। এতে করে দ্রুত পেজ লডিং হবে।</p>
+       </div>
+     <div className="card-info">
+       <h2>How useState Works</h2>
+       <p>useState হলো একটি হুক। যাতে কোনো উপাদান এর ভেরিয়েবল রাখতে পাড়ি। useState  হুক হলো একটি বিশেষ ফাংশন যা একটি আর্গুমেন্ট হিসাবে একটি ভেরিয়েবল এবং দুটি এন্টির মাধ্যমে এ্যারে প্রদান করে।</p>
+       </div>
     </div>
+    
   );
 }
 
--- src/Component/Card/Product/Shop/Shop.js
+++ src/Component/Card/Product/Shop/Shop.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Card from '../../../Card';
+import Random from '../../../Random/Random';
 import Navbar from './Navbar/Navbar';
 import './Shop.css';
 
 const Shop = () => {
     const [products, setProduct] = useState([]);
     const [cart, setCart] = useState([]);
+ 
 
 
     const handleAddToCart = (data) => {
@@ -20,28 +22,37 @@ const Shop = () => {
         .then(data => setProduct(data))
     },[])
 
-    
+
     return (
         <div>
             <Navbar/>
             
             <div className="shop-container">
-            <div className="product-container">
-
-            {products.map((product) => 
-                <div key={product.id}> 
-                  <Card data={product} handleAddToCart = {handleAddToCart}/>  
-                </div>)
-            }
-            </div>
+                <div className="product-container">
+                    {products.map((product) => 
+                        <div key={product.id}> 
+                        <Card data={product} handleAddToCart = {handleAddToCart}/>  
+                        </div>)
+                    }
+                    </div>
 
 
             <div className="item-container">
+                <div>
+                    <h3>Product Information</h3> 
                 { cart.map((item) => (
-                        <h3 key={item.id}>Name: {item.name}</h3>
+                        
+                    <div key={item.id} className="item-img">
+                        <img src={item.image} alt="" />
+                            <div >{item.name}</div>
+                        </div>
+                        
                     ))
                 }
-                <button className="button">Choose One</button>
+                <button className="info-btn">Choose</button>
+                </div>
+               
+              
             </div>
 
 
